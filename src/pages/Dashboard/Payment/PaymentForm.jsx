47e0482901f82b141698c6c7b38f14ab7e06bcc7
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-03-04 02:58:47 +0600

    Added error state

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,11 +1,14 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 
 const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
+    const [error, setError] = useState('');
 
-    const handleSubmit = e => {
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         if (!stripe || !elements) {
             return;
@@ -16,16 +19,32 @@ const PaymentForm = () => {
         if (!card) {
             return;
         }
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if (error) {
+            setError(error.message)
+        } else {
+            setError('');
+            console.log('Payment method:', paymentMethod)
+        }
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <CardElement>
-                    <button type="submit" disabled={!stripe}>
-                        Pay
-                    </button>
+        <div className="w-full">
+            <form onSubmit={handleSubmit} className="space-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto">
+                <CardElement className="p-2 border rounded">
+
                 </CardElement>
+                <button className="border-2 border-primary bg-primary text-black px-5 py-2 rounded-lg  font-bold hover:opacity-90 transition cursor-pointer w-full" type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+                {
+                    error && <p className="text-red-500">{error}</p>
+                }
             </form>
         </div>
     );

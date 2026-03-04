a41abd37b0d11c143cc0081527a2c62de3307919
ahrafi16 <anjumhossainrafi16@gmail.com>
2026-03-05 02:15:26 +0600

    Payment gateway funcitons created

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,16 +1,34 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import { useState } from "react";
 import { useParams } from "react-router";
+import useAxiosSecure from "../../../../src/hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../shared/loading/Loading";
 
 
 const PaymentForm = () => {
     const stripe = useStripe();
     const elements = useElements();
     const { parcelId } = useParams();
-    console.log("parcel Id : ", parcelId)
-
+    const axiosSecure = useAxiosSecure();
     const [error, setError] = useState('');
 
+    const { data: parcelInfo = {}, isPending } = useQuery({
+        queryKey: ['parcels', parcelId],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/parcels/${parcelId}`);
+            return res.data;
+        }
+    })
+
+    if (isPending) {
+        return <Loading></Loading>
+    }
+
+    console.log(parcelInfo);
+    const amount = parcelInfo.delivery_cost;
+    const amountInCents = amount * 100;
+    console.log(amountInCents)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -35,6 +53,29 @@ const PaymentForm = () => {
             setError('');
             console.log('Payment method:', paymentMethod)
         }
+
+        // create payment intent
+        const res = await axiosSecure.post('/create-payment-intent', {
+            amountInCents,
+            parcelId
+        })
+        const clientSecret = res.data.clientSecret;
+        const result = await stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement),
+                billing_details: {
+                    name: "Jenny"
+                }
+            }
+        });
+
+        if (result.error) {
+            console.log(result.error.message);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                console.log("Payment Succeeded!");
+            }
+        }
     }
 
 
@@ -45,7 +86,7 @@ const PaymentForm = () => {
 
                 </CardElement>
                 <button className="border-2 border-primary bg-primary text-black px-5 py-2 rounded-lg  font-bold hover:opacity-90 transition cursor-pointer w-full" type="submit" disabled={!stripe}>
-                    Pay
+                    Pay  ৳{amount}
                 </button>
                 {
                     error && <p className="text-red-500">{error}</p>
